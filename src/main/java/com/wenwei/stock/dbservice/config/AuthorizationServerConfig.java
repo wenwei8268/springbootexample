46fe0b5dcdd41399f55b66148a54e279d527d04e
wenweizww@wenweideMBP <zhou_wenwei@wuxiapptec.com>
2017-12-28 17:12:15 +0800

    init oauth2

--- src/main/java/com/wenwei/stock/dbservice/config/AuthorizationServerConfig.java
+++ src/main/java/com/wenwei/stock/dbservice/config/AuthorizationServerConfig.java
@@ -1,63 +1,63 @@
-//package com.wenwei.stock.dbservice.config;
-//
-//
-//import com.netflix.discovery.converters.Auto;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.redis.connection.RedisConnectionFactory;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-//import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-//import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-//import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
-//
-///**
-// * author:zhou_wenwei
-// * mail:
-// * date:2017/12/25
-// * description:
-// */
-//@Configuration
-//@EnableAuthorizationServer
-//public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
-//
-//    @Autowired
-//    private RedisConnectionFactory connectionFactory;
-//
-//    @Autowired
-//    private AuthenticationManager authenticationManager;
-//
-//    private UserDetailsService userDetailsService;
-//
-//    public RedisTokenStore tokenStore(){
-//        return new RedisTokenStore(connectionFactory);
-//    }
-//    @Override
-//    public void configure(AuthorizationServerEndpointsConfigurer endpoints){
-//        endpoints.authenticationManager(authenticationManager).userDetailsService(userDetailsService).tokenStore(tokenStore());
-//    }
-//
-//    @Override
-//    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-//        security
-//                .tokenKeyAccess("permitAll()")
-//                .checkTokenAccess("isAuthenticated()");
-//    }
-//
-//    @Override
-//    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-//        clients.inMemory()
-//                .withClient("android")
-//                .scopes("xx")
-//                .secret("android")
-//                .authorizedGrantTypes("password", "authorization_code", "refresh_token")
-//                .and()
-//                .withClient("webapp")
-//                .scopes("xx")
-//                .authorizedGrantTypes("implicit");
-//    }
-//
-//}
+package com.wenwei.stock.dbservice.config;
+
+
+import com.netflix.discovery.converters.Auto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
+
+/**
+ * author:zhou_wenwei
+ * mail:
+ * date:2017/12/25
+ * description:
+ */
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private RedisConnectionFactory connectionFactory;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    private UserDetailsService userDetailsService;
+
+    public RedisTokenStore tokenStore(){
+        return new RedisTokenStore(connectionFactory);
+    }
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints){
+        endpoints.authenticationManager(authenticationManager).userDetailsService(userDetailsService).tokenStore(tokenStore());
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security
+                .tokenKeyAccess("permitAll()")
+                .checkTokenAccess("isAuthenticated()");
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("android")
+                .scopes("xx")
+                .secret("android")
+                .authorizedGrantTypes("password", "authorization_code", "refresh_token")
+                .and()
+                .withClient("webapp")
+                .scopes("xx")
+                .authorizedGrantTypes("implicit");
+    }
+
+}
